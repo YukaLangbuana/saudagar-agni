d4bb2d5b5d28e57dd3474b4841698b63c0203df2
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-04-22 00:53:59 -0700

    heroku deployment

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_cors import CORS
+import os
 
 #APP
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yukalangbuana:yukalangbuana@localhost:5432/newagni'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yukalangbuana:yukalangbuana@localhost:5432/newagni'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 #DB
 db = SQLAlchemy(app)
